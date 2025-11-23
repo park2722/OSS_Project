06e6afa03f8d40537873f73f86f0737b7c11bd8d
Rusan <moonbak0744@naver.com>
2025-11-23 19:37:56 +0900

    Modify) korea_Shortest_Map_Search을 main실행 파일로 설정

--- korea_Shortest_Map_Search.py
+++ korea_Shortest_Map_Search.py
@@ -1,19 +1,4 @@
-import matplotlib.pyplot as plt
-import cv2
-from graph_core import CityGraph
+from GUI import RouteWindow
 
-#이미지 저장 위치로 변경 필요
-image = cv2.imread("./korea_map.png")
-height, width = image.shape[:2]
-Scale_up = cv2.resize(image, None, None,2,2, cv2.INTER_CUBIC)
-plt.figure(figsize=(10, 7))
-plt.imshow(image)
-
-myGraph = CityGraph()
-CityGraph.load_data(myGraph)
-CityGraph.print_data(myGraph)
-
-x_values = [myGraph.City_pos[city][0] for city in myGraph.City_pos]
-y_values = [myGraph.City_pos[city][1] for city in myGraph.City_pos]
-plt.scatter(x_values, y_values, color="blue",s = 5)
-plt.show()
+app = RouteWindow()
+app.mainloop()
